183ca972880ed28f6d6c22f69e16cf4311bd6262
Luis Felipe Schenone <schenonef@gmail.com>
2014-11-03 17:44:55 -0300

    More Facebook login testing

--- js/facebook.js
+++ js/facebook.js
@@ -7,11 +7,32 @@ $( function () {
 		version: 'v2.1'
 	});
 	
-/*
 	FB.Event.subscribe( 'auth.authResponseChange', onAuthResponseChange );
 	FB.Event.subscribe( 'auth.statusChange', onStatusChange );
-*/
 });
+
+function login( callback ) {
+	FB.login( callback );
+}
+
+function loginCallback( response ) {
+	console.log( 'loginCallback', response );
+	if ( response.status != 'connected' ) {
+		top.location.href = 'https://www.facebook.com/appcenter/YOUR_APP_NAMESPACE';
+	}
+}
+
+function onStatusChange( response ) {
+	if ( response.status != 'connected' ) {
+		login( loginCallback );
+	} else {
+		showHome();
+	}
+}
+
+function onAuthResponseChange( response ) {
+	console.log( 'onAuthResponseChange', response );
+}
 
 /*
 function statusChangeCallback( response ) {
