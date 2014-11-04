17d43bc148c0ccf19bdb75b436dcf6ed1ebef799
Luis Felipe Schenone <schenonef@gmail.com>
2014-11-04 09:38:56 -0300

    Facebook login testing

--- includes/config.php
+++ includes/config.php
@@ -7,6 +7,9 @@ error_reporting( E_ALL );
 //General settings
 define( 'BASE', 'http://localhost/pixel-by-pixel/' );
 define( 'TITLE', 'Pixel by Pixel' );
+define( 'DEFAULT_CONTROLLER', 'board' );
+define( 'DEFAULT_METHOD', 'view' );
+define( 'CONTACT_EMAIL', 'contact@pixel-by-pixel.app' );
 
 //Database settings
 define( 'DB_HOST', 'localhost' );
@@ -14,9 +17,6 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASS', 'penta' );
 define( 'DB_NAME', 'pbp' );
 
-
-define( 'DEFAULT_CONTROLLER', 'board' );
-define( 'DEFAULT_METHOD', 'view' );
-
-
-define( 'CONTACT_EMAIL', 'contact@pixel-by-pixel.com' );
+//Facebook app
+define( 'FACEBOOK_APP_ID', '707049712677506' );
+define( 'FACEBOOK_APP_SECRET', '31ab8adb0f1da9eff3a489b1e7eb7435' );
