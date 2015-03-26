2f3cf970018649853d93e93266c329c70c2ab026
Felipe Schenone <schenonef@gmail.com>
2015-03-26 00:16:58 -0300

    Fix

--- includes/config.php
+++ includes/config.php
@@ -1,16 +1,16 @@
 <?php
 
-//Debugging
+// Debugging
 ini_set( 'display_errors', 1 );
 error_reporting( E_ALL );
 
-//General settings
-define( 'BASE', 'http://local.pixel-by-pixel.net/pixel-by-pixel/' );
+// General settings
+define( 'BASE', 'http://pixel-by-pixel.net/' );
 define( 'TITLE', 'Pixel by Pixel' );
 define( 'DESCRIPTION', 'An infinite canvas for collaborative pixel art. Come leave your mark!' );
-define( 'CONTACT_EMAIL', 'contact@pixel-by-pixel.app' );
+define( 'CONTACT_EMAIL', 'contact@pixel-by-pixel.net' );
 
-//Defaults
+// Defaults
 define( 'DEFAULT_CONTROLLER', 'Board' );
 define( 'DEFAULT_METHOD', 'view' );
 define( 'DEFAULT_TOPLEFTX', 0 );
@@ -18,12 +18,12 @@ define( 'DEFAULT_TOPLEFTY', 0 );
 define( 'DEFAULT_PIXELSIZE', 4 );
 date_default_timezone_set( 'America/Argentina/Buenos_Aires' );
 
-//Database settings
+// Database settings
 define( 'DB_HOST', 'localhost' );
 define( 'DB_USER', 'root' );
-define( 'DB_PASS', 'penta' );
+define( 'DB_PASS', '' );
 define( 'DB_NAME', 'pixel-by-pixel' );
 
-//Facebook app
-define( 'FACEBOOK_APP_ID', '707049712677506' );
-define( 'FACEBOOK_APP_SECRET', '2e44dccf08c4631809022e1b0bc43f4e' );
+// Facebook app
+define( 'FACEBOOK_APP_ID', '' );
+define( 'FACEBOOK_APP_SECRET', '' );
