05b34098374eac6dfe54befa00ff09cb098e6bc9
Felipe Schenone <schenonef@gmail.com>
2015-06-15 01:07:55 +0000

    Doing it right

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,11 @@
 <?php
 
 // Debugging
-ini_set( 'display_errors', 1 );
+ini_set( 'display_errors', 0 );
 error_reporting( E_ALL );
 
 // General settings
-define( 'BASE', 'https://pixelbypixel-lfschenone.c9.io/' );
+define( 'BASE', 'http://pixelbypixel.co/' );
 define( 'TITLE', 'Pixel by Pixel' );
 define( 'DESCRIPTION', 'An infinite canvas of collaborative pixel art. Come leave your mark!' );
 date_default_timezone_set( 'America/Argentina/Buenos_Aires' );
@@ -16,12 +16,12 @@ define( 'DEFAULT_CENTERY', 0 );
 define( 'DEFAULT_PIXELSIZE', 2 );
 
 // Database
-define( 'DB_HOST', getenv('IP') );
-define( 'DB_USER', getenv('C9_USER') );
+define( 'DB_HOST', '' );
+define( 'DB_USER', '' );
 define( 'DB_PASS', '' );
-define( 'DB_NAME', 'c9' );
+define( 'DB_NAME', '' );
 define( 'DB_PORT', 3306 );
 
 // Facebook
-define( 'FACEBOOK_APP_ID', '707049712677506' );
-define( 'FACEBOOK_APP_SECRET', '2e44dccf08c4631809022e1b0bc43f4e' );
+define( 'FACEBOOK_APP_ID', '' );
+define( 'FACEBOOK_APP_SECRET', '' );
