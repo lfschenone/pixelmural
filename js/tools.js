13a97197a9b8191097b7aa8ea1b22b2f85ae2f1e
Felipe Schenone <schenonef@gmail.com>
2015-10-05 17:20:51 -0300

    Fixes

--- js/tools.js
+++ js/tools.js
@@ -74,9 +74,9 @@ tools = {
 	},
 
 	clickMoveButton: function () {
-		mouse.downAction = mouse.moveMural1;
-		mouse.dragAction = mouse.moveMural2;
-		mouse.upAction = mouse.moveMural3;
+		mouse.downAction = move.down;
+		mouse.dragAction = move.drag;
+		mouse.upAction = move.up;
 		tools.activeTool = 'move';
 		tools.update();
 	},
@@ -131,8 +131,8 @@ tools = {
 	},
 
 	clickDropperButton: function () {
-		mouse.downAction = mouse.suckColor;
-		mouse.dragAction = mouse.suckColor;
+		mouse.downAction = dropper.suckColor;
+		mouse.dragAction = dropper.suckColor;
 		mouse.upAction = null;
 		tools.previousTool = tools.activeTool;
 		tools.activeTool = 'dropper';
@@ -140,9 +140,9 @@ tools = {
 	},
 
 	clickPencilButton: function () {
-		mouse.downAction = mouse.moveMural1;
-		mouse.dragAction = mouse.moveMural2;
-		mouse.upAction = mouse.paintPixel;
+		mouse.downAction = pencil.down;
+		mouse.dragAction = pencil.drag;
+		mouse.upAction = pencil.up;
 		tools.activeTool = 'pencil';
 		tools.update();
 	},
@@ -161,17 +161,17 @@ tools = {
 	},
 
 	clickEraserButton: function () {
-		mouse.downAction = mouse.erasePixel;
-		mouse.dragAction = mouse.erasePixel;
+		mouse.downAction = eraser.erasePixel;
+		mouse.dragAction = eraser.erasePixel;
 		mouse.upAction = null;
 		tools.activeTool = 'eraser';
 		tools.update();
 	},
 
 	clickBucketButton: function () {
-		mouse.downAction = mouse.paintArea;
+		mouse.downAction = null;
 		mouse.dragAction = null;
-		mouse.upAction = null;
+		mouse.upAction = bucket.paintArea;
 		tools.activeTool = 'bucket';
 		tools.update();
 	},
@@ -222,6 +222,165 @@ tools = {
 		$( '#' + tools.activeTool + '-button' ).addClass( 'active' );
 
 		$( '#color-input' ).spectrum( 'set', tools.color );
+	}
+};
+
+move = {
+
+	down: function () {
+		mouse.diffX = 0;
+		mouse.diffY = 0;
+		mural.imageData = mural.context.getImageData( 0, 0, mural.width, mural.height );
+	},
+
+	drag: function ( event ) {
+		mural.centerX += mouse.previousX - mouse.currentX;
+		mural.centerY += mouse.previousY - mouse.currentY;
+
+		mouse.diffX += ( mouse.currentX - mouse.previousX ) * mural.pixelSize;
+		mouse.diffY += ( mouse.currentY - mouse.previousY ) * mural.pixelSize;
+
+		mural.context.clearRect( 0, 0, mural.width, mural.height );
+		mural.context.putImageData( mural.imageData, parseFloat( mouse.diffX ), parseFloat( mouse.diffY ) );
+
+		// Bugfix: without this, the mural flickers when moving, not sure why
+		mouse.currentX = mouse.getCurrentX( event );
+		mouse.currentY = mouse.getCurrentY( event );
+	},
+
+	up: function ( event ) {
+		if ( mouse.diffX || mouse.diffY ) {
+			mural.update();
+			preview.update();
+			return;
+		}
+		var data = { 'x': mouse.currentX, 'y': mouse.currentY };
+		$.get( 'Pixels', data, function ( response ) {
+			if ( response ) {
+				showPixelAuthor( response.Pixel, response.Author );
+			}
+		});
+	}
+};
+
+pencil = {
+
+	down: function () {
+		mouse.diffX = 0;
+		mouse.diffY = 0;
+		mural.imageData = mural.context.getImageData( 0, 0, mural.width, mural.height );
+	},
+
+	drag: function ( event ) {
+		mural.centerX += mouse.previousX - mouse.currentX;
+		mural.centerY += mouse.previousY - mouse.currentY;
+
+		mouse.diffX += ( mouse.currentX - mouse.previousX ) * mural.pixelSize;
+		mouse.diffY += ( mouse.currentY - mouse.previousY ) * mural.pixelSize;
+
+		mural.context.clearRect( 0, 0, mural.width, mural.height );
+		mural.context.putImageData( mural.imageData, parseFloat( mouse.diffX ), parseFloat( mouse.diffY ) );
+
+		// Bugfix: without this, the mural flickers when moving, not sure why
+		mouse.currentX = mouse.getCurrentX( event );
+		mouse.currentY = mouse.getCurrentY( event );
+	},
+
+	up: function ( event ) {
+		if ( mouse.diffX || mouse.diffY ) {
+			mural.update();
+			preview.update();
+			return;
+		}
+
+		var oldPixel = mural.getPixel( mouse.currentX, mouse.currentY ),
+			newPixel = new Pixel({ 'x': mouse.currentX, 'y': mouse.currentY, 'color': tools.color });
+
+		if ( newPixel.color === oldPixel.color && mouse.currentX === mouse.previousX && mouse.currentY === mouse.previousY ) {
+			newPixel.color = null; // For convenience, re-painting a pixel erases it
+		}
+
+		newPixel.paint().save().register( oldPixel );
+	}
+};
+
+eraser = {
+
+	erasePixel: function ( event ) {
+		var oldPixel = mural.getPixel( mouse.currentX, mouse.currentY );
+
+		if ( oldPixel.color === null ) {
+			return; // The pixel doesn't exist, no need to continue
+		}
+
+		var newPixel = new Pixel({ 'x': mouse.currentX, 'y': mouse.currentY });
+
+		newPixel.erase().save().register( oldPixel );
+	}
+};
+
+dropper = {
+
+	suckColor: function ( event ) {
+		var offsetX = event.pageX - $( event.target ).offset().left - 1; // The -1 is to correct a minor displacement
+			offsetY = event.pageY - $( event.target ).offset().top - 2, // The -2 is to correct a minor displacement
+			imageData = mural.context.getImageData( offsetX, offsetY, 1, 1 ),
+			red   = imageData.data[0],
+			green = imageData.data[1],
+			blue  = imageData.data[2],
+			alpha = imageData.data[3];
+		if ( !alpha ) {
+			return; // The user clicked the background
+		}
+		tools.color = rgb2hex( red, green, blue );
+		tools.update();
+	}
+};
+
+bucket = {
+
+	paintArea: function ( event ) {
+		showLoading();
+		var data = {
+			'x': mouse.currentX,
+			'y': mouse.currentY,
+			'color': tools.color
+		};
+		$.post( 'Areas', data, function ( response ) {
+			//console.log( response );
+			switch ( response.code ) {
+				case 200:
+					var newArea = new window.Area,
+						newPixelData,
+						newPixel,
+						oldArea = new window.Area,
+						oldPixelData,
+						oldPixel;
+					for ( var i = 0; i < response.newAreaData.length; i++ ) {
+						newPixelData = response.newAreaData[ i ];
+						newPixel = new window.Pixel( newPixelData );
+						newArea.pixels.push( newPixel );
+	
+						oldPixelData = response.oldAreaData[ i ];
+						oldPixel = new window.Pixel( oldPixelData );
+						oldArea.pixels.push( oldPixel );
+					}
+					newArea.paint().register( oldArea );
+					break;
+
+				case 401:
+				case 403:
+					if ( response.data ) {
+						var Pixel = new window.Pixel( response.data );
+						Pixel.paint().unregister();
+					} else {
+						var Pixel = new window.Pixel({ 'x': data.x, 'y': data.y });
+						Pixel.erase().unregister();
+					}
+					break;
+			}
+			hideLoading();
+		});
 	}
 };
 
