a1ea810c8869b4df2ad4c274099d8e6deb3d6c75
Felipe Schenone <schenonef@gmail.com>
2015-10-05 14:10:14 -0300

    Fixes

--- js/functions.js
+++ js/functions.js
@@ -17,15 +17,12 @@ function hideLoading() {
 function showPixelAuthor( Pixel, Author ) {
 	var picture = '<img src="images/anon.png">',
 		author = Author.name,
-		link = '<br>';
+		link = Author.link ? '<br><a href="' + Author.link + '/">' + Author.link + '</a>' : '';
 		date = new Date( Pixel.insert_time * 1000 ),
 		date = '<br>' + date.toUTCString();
 	if ( Author.facebook_id ) {
 		picture = '<img src="http://graph.facebook.com/' + Author.facebook_id + '/picture">';
 		author = '<a href="https://www.facebook.com/app_scoped_user_id/' + Author.facebook_id + '/">' + Author.name + '</a>';
-	}
-	if ( Author.link ) {
-		link += '<a href="' + Author.link + '/">' + Author.link + '</a>';
 	}
 	var span = $( '<span>' ).attr( 'id', 'author' ).html( picture + author + link + date );
 	$( 'body' ).append( span );
